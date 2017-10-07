cf91ad6144ad1dc04170d2020f6d4d50da3a0dc1
Ashwin Jeyaseelan <ashwinjey@gmail.com>
2017-10-07 14:07:27 -0400

    Started custom bot

---
+++ src/Player/GigaBot.hs
@@ -0,0 +1,9 @@
+module Player.GigaBot (playerGigaBot) where
+
+import Types
+
+teamMembers :: String
+teamMembers = "Ashwin Jeyaseelan Sang-Kyun Han"
+
+playerGigaBot :: Player
+playerGigaBot = error "Define me!"
