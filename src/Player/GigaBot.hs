9c43078f5b042d8b28ca0ebb9168395de9756531
Ashwin Jeyaseelan <ashwinjey@gmail.com>
2017-10-07 14:52:49 -0400

    Starting algorithm

--- src/Player/GigaBot.hs
+++ src/Player/GigaBot.hs
@@ -1,9 +1,13 @@
 module Player.GigaBot (playerGigaBot) where
 
 import Types
+import Checks
 
 teamMembers :: String
 teamMembers = "Ashwin Jeyaseelan Sang-Kyun Han"
 
 playerGigaBot :: Player
-playerGigaBot = error "Define me!"
+playerGigaBot = Player strategy "Future"
+
+strategy :: Tile -> Board -> IO Move
+strategy tile board = error "Define me!"
